0d3fd5c455d7db33fa93c4a38eb85ae2f51b4ce3
whykitsune <145267739+whykitsune@users.noreply.github.com>
2023-11-10 17:23:12 +0300

    added full lab 3

---
+++ lab 3/main.js
@@ -0,0 +1,15 @@
+function list() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
